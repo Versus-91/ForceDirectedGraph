214ed12e8de71a28354bdb0885b93b5eeb51f71c
Glitch (infovis-a3) <none>
2023-06-01 06:29:12 +0000

    🐴🏰 Checkpoint ./script.js:57809084/7682 ./style.js:57809084/5239 ./index.html:57809084/2667 ./style.css:57809084/748

--- script.js
+++ script.js
@@ -3,12 +3,117 @@
 
 /* global fetch, cytoscape */
 import _style from "/style.js";
+// create 2 data_set
+const data1 = [
+  { group: "A", value: 4 },
+  { group: "B", value: 16 },
+  { group: "C", value: 8 },
+];
+
+const data2 = [
+  { group: "A", value: 7 },
+  { group: "B", value: 1 },
+  { group: "C", value: 20 },
+];
+
+// set the dimensions and margins of the graph
+const margin = { top: 1, right: 1, bottom: 3, left: 2 },
+  width = 40 - margin.left - margin.right,
+  height = 32 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+const svg = d3
+  .select("#my_dataviz")
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", `translate(${margin.left},${margin.top})`);
+
+// X axis
+const x = d3
+  .scaleBand()
+  .range([0, width])
+  .domain(data1.map((d) => d.group))
+  .padding(0.2);
+svg
+  .append("g")
+  .attr("transform", `translate(0,${height})`)
+  .call(d3.axisBottom(x));
+
+// Add Y axis
+const y = d3.scaleLinear().domain([0, 20]).range([height, 0]);
+svg.append("g").attr("class", "myYaxis").call(d3.axisLeft(y));
+
+// A function that create / update the plot for a given variable:
+function update(data) {
+  var u = svg.selectAll("rect").data(data);
+
+  u.join("rect")
+    .transition()
+    .duration(1000)
+    .attr("x", (d) => x(d.group))
+    .attr("y", (d) => y(d.value))
+    .attr("width", x.bandwidth())
+    .attr("height", (d) => height - y(d.value))
+    .attr("fill", "#69b3a2");
+}
+
+// Initialize the plot with the first dataset
+update(data1);
+function getImage() {
+  //get svg element.
+  var svgElement = document.getElementById("my_dataviz");
+
+  var svgData = svgElement.innerHTML;
+  return "data:image/svg+xml;charset=utf-8,"+encodeURIComponent(svgData);
+
+}
 
 // returns true if the point "p" is inside the circle defined by "c" (center) and "r" (radius)
 function isInCircle(c, r, p) {
-  return Math.pow(p.x - c.x, 2) + Math.pow(p.y - c.y, 2) <= Math.pow(r, 2) 
+  return Math.pow(p.x - c.x, 2) + Math.pow(p.y - c.y, 2) <= Math.pow(r, 2);
+}
+const rangeSelector = document.getElementById("rangeSelector");
+const lens = document.getElementById("lens");
+let radius = 40;
+let radiusMultiplier = 0;
+let changeNodeShape = false;
+let highlightEdge = false;
+let lenseBehavoirSelect = document.getElementById("lenseBehavoirSelect");
+
+lenseBehavoirSelect.addEventListener("change", (e) => {
+  getImage();
+  let selectedValue = Number(e.target.value);
+  if (selectedValue === 1) {
+    changeNodeShape = true;
+    highlightEdge = false;
+  } else if (selectedValue === 2) {
+    changeNodeShape = false;
+    highlightEdge = true;
+  }
+});
+if (rangeSelector) {
+  rangeSelector.addEventListener("change", (e) => {
+    radiusMultiplier = e.target.value;
+    radius = 40 + 40 * radiusMultiplier;
+    lens.setAttribute("r", radius);
+  });
 }
 
+document.addEventListener("keydown", (event) => {
+  if (event.key === "+") {
+    radiusMultiplier += 0.2;
+    radius = 40 + 40 * radiusMultiplier;
+    lens.setAttribute("r", radius);
+  } else if (event.key === "-") {
+    radiusMultiplier -= 0.2;
+    radius = 40 + 40 * radiusMultiplier;
+    if (radius >= 40) {
+      lens.setAttribute("r", radius);
+    }
+  }
+});
 fetch("data/data.json")
   .then((res) => res.json())
   .then((data) => {
@@ -36,14 +141,53 @@ fetch("data/data.json")
         2. if you experience performance issues, use cy.startBatch() and cy.endBatch() to avoid unnecessary canvas redraws. See https://js.cytoscape.org/#cy.batch for more
         3. see below how to get the mouse and node positions
       */
-      
+
       const mouse = { x: e.originalEvent.x, y: e.originalEvent.y };
-      console.log(`Mouse position: [x: ${mouse.x}, y: ${mouse.y}]`);
-      
+      // console.log(`Mouse position: [x: ${mouse.x}, y: ${mouse.y}]`);
+      if (lens) {
+        lens.setAttribute("cx", mouse.x);
+        lens.setAttribute("cy", mouse.y);
+      }
       cy.nodes().forEach((n) => {
+        cy.startBatch();
         const node = n.renderedPosition(); // Careful: other position functions may invoke different coordinate systems
+        let getNode = cy.$(`#${n.id()}`);
+        if (isInCircle(mouse, radius, node)) {
+          if (!!getNode) {
+            if (changeNodeShape) {
+               getNode.addClass("magic");
+              // getNode.style({
+              //   'background-image': "data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='10' height='10'><linearGradient id='gradient'><stop offset='10%' stop-color='%23F00'/><stop offset='90%' stop-color='%23fcc'/> </linearGradient><rect fill='url(%23gradient)' x='0' y='0' width='100%' height='100%'/></svg>",
+              //   'background-width': '100%', // Adjust the width and height according to your needs
+              //   'background-height': '100%'
+              // });
+            }
+            if (highlightEdge) {
+              let edges = getNode.connectedEdges();
+              edges.forEach((e) => {
+                let edge = cy.$(`#${e.id()}`);
+                if (!!edge) {
+                  edge.addClass("magic");
+                }
+              });
+            }
+          }
+        } else {
+          if (changeNodeShape) {
+            getNode.removeClass("magic");
+          }
+          if (highlightEdge) {
+            let edges = getNode.connectedEdges();
+            edges.forEach((e) => {
+              let edge = cy.$(`#${e.id()}`);
+              if (!!edge) {
+                edge.removeClass("magic");
+              }
+            });
+          }
+        }
+        cy.endBatch();
         // console.log(`Node position: [x: ${node.x}, y: ${node.y}]`);
       });
-      
     });
   });
