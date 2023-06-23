559080b68587e1bff18742a2fb94f7336636016f
Glitch (wandering-opalescent-mayonnaise) <none>
2023-06-23 19:44:06 +0000

    🐂🌹 Checkpoint ./index.html:57809084/63 ./script.js:57809084/2208

--- script.js
+++ script.js
@@ -4,7 +4,77 @@
 /* global fetch, cytoscape */
 import _style from "/style.js";
 
+var data1 = [
+  {group: "A", value: 4},
+  {group: "B", value: 16},
+  {group: "C", value: 8}
+];
 
+var data2 = [
+  {group: "A", value: 7},
+  {group: "B", value: 1},
+  {group: "C", value: 20}
+];
+
+// set the dimensions and margins of the graph
+var margin = {top: 30, right: 30, bottom: 70, left: 60},
+   width = 460 - margin.left - margin.right,
+   height = 400 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("#my_dataviz")
+ .append("svg")
+   .attr("width", width + margin.left + margin.right)
+   .attr("height", height + margin.top + margin.bottom)
+ .append("g")
+   .attr("transform",
+         "translate(" + margin.left + "," + margin.top + ")");
+
+// X axis
+var x = d3.scaleBand()
+ .range([ 0, width ])
+ .domain(data1.map(function(d) { return d.group; }))
+ .padding(0.2);
+svg.append("g")
+ .attr("transform", "translate(0," + height + ")")
+ .call(d3.axisBottom(x))
+
+// Add Y axis
+var y = d3.scaleLinear()
+ .domain([0, 20])
+ .range([ height, 0]);
+svg.append("g")
+ .attr("class", "myYaxis")
+ .call(d3.axisLeft(y));
+
+// A function that create / update the plot for a given variable:
+function update(data) {
+ var u = svg.selectAll("rect")
+   .data(data)
+
+ u
+   .enter()
+   .append("rect")
+   .merge(u)
+   .transition()
+   .duration(1000)
+     .attr("x", function(d) { return x(d.group); })
+     .attr("y", function(d) { return y(d.value); })
+     .attr("width", x.bandwidth())
+     .attr("height", function(d) { return height - y(d.value); })
+     .attr("fill", "#69b3a2")
+}
+// Initialize the plot with the first dataset
+update(data1)
+
+function getImage() {
+  var chartContainer = d3.select("#my_dataviz");
+  var svgElement = chartContainer.select("svg").node();
+
+  var svgString = new XMLSerializer().serializeToString(svgElement);
+
+ return "data:image/svg+xml;base64," + btoa(svgString);
+}
 // returns true if the point "p" is inside the circle defined by "c" (center) and "r" (radius)
 function isInCircle(c, r, p) {
   return Math.pow(p.x - c.x, 2) + Math.pow(p.y - c.y, 2) <= Math.pow(r, 2);
@@ -92,8 +162,8 @@ fetch("data/data.json")
             if (changeNodeShape) {
               graphNode.addClass("magic");
               graphNode.style({
-                 'background-image': "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMDAiIGhlaWdodD0iMTAwIj48cmVjdCB4PSIxMCIgeT0iMTAiIHdpZHRoPSI4MCIgaGVpZ2h0PSI4MCIgc3R5bGU9ImZpbGw6IG9yYW5nZTsiPjwvcmVjdD48L3N2Zz4=",
-                 'background-width': '100%', // Adjust the width and height according to your needs
+                'background-image': "url(" + getImage() + ") no-repeat center",
+                'background-width': '100%', // Adjust the width and height according to your needs
                  'background-height': '100%'
               });
             }
