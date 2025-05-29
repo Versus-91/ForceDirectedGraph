9e96ea9615c642d6c00c841392d366ce80a278d1
Glitch (wandering-opalescent-mayonnaise) <none>
2025-05-29 10:50:30 +0200

    fixing path

--- script.js
+++ script.js
@@ -5,64 +5,64 @@
 import _style from "/style.js";
 
 var data1 = [
-  {group: "A", value: 4},
-  {group: "B", value: 16},
-  {group: "C", value: 8}
+  { group: "A", value: 4 },
+  { group: "B", value: 16 },
+  { group: "C", value: 8 }
 ];
 
 var data2 = [
-  {group: "A", value: 7},
-  {group: "B", value: 1},
-  {group: "C", value: 20}
+  { group: "A", value: 7 },
+  { group: "B", value: 1 },
+  { group: "C", value: 20 }
 ];
 
 // set the dimensions and margins of the graph
-var margin = {top: 30, right: 30, bottom: 70, left: 60},
-   width = 460 - margin.left - margin.right,
-   height = 400 - margin.top - margin.bottom;
+var margin = { top: 30, right: 30, bottom: 70, left: 60 },
+  width = 460 - margin.left - margin.right,
+  height = 400 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg = d3.select("#my_dataviz")
- .append("svg")
-   .attr("width", width + margin.left + margin.right)
-   .attr("height", height + margin.top + margin.bottom)
- .append("g")
-   .attr("transform",
-         "translate(" + margin.left + "," + margin.top + ")");
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform",
+    "translate(" + margin.left + "," + margin.top + ")");
 
 // X axis
 var x = d3.scaleBand()
- .range([ 0, width ])
- .domain(data1.map(function(d) { return d.group; }))
- .padding(0.2);
+  .range([0, width])
+  .domain(data1.map(function (d) { return d.group; }))
+  .padding(0.2);
 svg.append("g")
- .attr("transform", "translate(0," + height + ")")
- .call(d3.axisBottom(x))
+  .attr("transform", "translate(0," + height + ")")
+  .call(d3.axisBottom(x))
 
 // Add Y axis
 var y = d3.scaleLinear()
- .domain([0, 20])
- .range([ height, 0]);
+  .domain([0, 20])
+  .range([height, 0]);
 svg.append("g")
- .attr("class", "myYaxis")
- .call(d3.axisLeft(y));
+  .attr("class", "myYaxis")
+  .call(d3.axisLeft(y));
 
 // A function that create / update the plot for a given variable:
 function update(data) {
- var u = svg.selectAll("rect")
-   .data(data)
+  var u = svg.selectAll("rect")
+    .data(data)
 
- u
-   .enter()
-   .append("rect")
-   .merge(u)
-   .transition()
-   .duration(1000)
-     .attr("x", function(d) { return x(d.group); })
-     .attr("y", function(d) { return y(d.value); })
-     .attr("width", x.bandwidth())
-     .attr("height", function(d) { return height - y(d.value); })
-     .attr("fill", "#69b3a2")
+  u
+    .enter()
+    .append("rect")
+    .merge(u)
+    .transition()
+    .duration(1000)
+    .attr("x", function (d) { return x(d.group); })
+    .attr("y", function (d) { return y(d.value); })
+    .attr("width", x.bandwidth())
+    .attr("height", function (d) { return height - y(d.value); })
+    .attr("fill", "#69b3a2")
 }
 // Initialize the plot with the first dataset
 update(data1)
@@ -73,7 +73,7 @@ function getImage() {
 
   var svgString = new XMLSerializer().serializeToString(svgElement);
 
- return "data:image/svg+xml;base64," + btoa(svgString);
+  return "data:image/svg+xml;base64," + btoa(svgString);
 }
 // returns true if the point "p" is inside the circle defined by "c" (center) and "r" (radius)
 function isInCircle(c, r, p) {
@@ -83,16 +83,16 @@ const rangeSelector = document.getElementById("rangeSelector");
 const lens = document.getElementById("lens");
 let radius = 40;
 let radiusMultiplier = 0;
-let changeNodeShape = true;
-let highlightEdge = false;
+let changeNodeShape = false;
+let highlightEdge = true;
 let lenseBehavoirSelect = document.getElementById("lenseBehavoirSelect");
 
 lenseBehavoirSelect.addEventListener("change", (e) => {
   let selectedValue = Number(e.target.value);
-  if (selectedValue === 1) {
+  if (selectedValue == 1) {
     changeNodeShape = true;
     highlightEdge = false;
-  } else if (selectedValue === 2) {
+  } else if (selectedValue == 2) {
     changeNodeShape = false;
     highlightEdge = true;
   }
@@ -164,7 +164,7 @@ fetch("data/data.json")
               graphNode.style({
                 'background-image': "url(" + getImage() + ") no-repeat center",
                 'background-width': '100%', // Adjust the width and height according to your needs
-                 'background-height': '100%'
+                'background-height': '100%'
               });
             }
             if (highlightEdge) {
